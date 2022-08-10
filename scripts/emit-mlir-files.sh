86c5577e016cab039189b8f720e1a4f07b859132
Renato Golin <rengolin@systemcall.eu>
2022-08-10 18:50:09 +0100

    some fixes for generating mlir files

--- scripts/emit-mlir-files.sh
+++ scripts/emit-mlir-files.sh
@@ -12,16 +12,22 @@ if [ -d "$PROJECT" ]; then
   cd "$PROJECT"
 fi
 
+ROOT=$(git rev-parse --show-toplevel)
+if [ ! -d "$ROOT" ]; then
+    echo "Cannot find repository root"
+    exit 1
+fi
+
 # Check the environment
-if [ ! -d "externals/torch-mlir/mlir_venv" ]; then
+if [ ! -d "external/torch-mlir/mlir_venv" ]; then
     echo "Torch-MLIR project not built"
     exit 1
 fi
 
 # For each front-end, enter its environment, run all the scripts
 FRONTENDS="torch-mlir"
-for FE in FRONTENDS; do
-    source "external/$FE/bin/activate"
+for FE in $FRONTENDS; do
+    source "external/$FE/mlir_venv/bin/activate"
     $ROOT/scripts/$FE-generate.sh
     deactivate
 done
--- scripts/torch-mlir-generate.sh
+++ scripts/torch-mlir-generate.sh
@@ -8,12 +8,12 @@ if [ ! -d "$ROOT" ]; then
     echo "Cannot find repository root"
     exit 1
 fi
-MODELS="$ROOT/front-ends/torch-mlir/models"
-MLIRs="$ROOT/front-ends/torch-mlir/mlir"
 
 # For each Python script, generate an MLIR file
 # with the same name and different extension
-for MODEL in $(find "$MODELS" -type f -name \*.py); do
-    OUT="${$MODEL%.py}.mlir"
-    python "$MODEL" > "$MLIR"
+pushd "$ROOT/front-ends/torch-mlir/models"
+for MODEL in $(find . -type f -name \*.py); do
+    OUT="${MODEL%.py}.mlir"
+    python "$MODEL" > "../mlir/$OUT"
 done
+popd
