a60b7a4206aaed0e919df3157fe9222c708bc2a9
Renato Golin <rengolin@systemcall.eu>
2022-08-11 15:58:58 +0100

    gitignore missed the lib directory

---
+++ front-ends/torch-mlir/lib/torch_mlir_compile.py
@@ -0,0 +1,10 @@
+# Implements the torch_mlir compiler steps into multiple MLIR outputs
+
+import torch
+import torch_mlir
+
+def torch_mlir_compile(model, input, output_formats=['torch', 'linalg-on-tensors', 'tosa', 'mhlo']):
+    pass
+    for format in output_formats:
+        module = torch_mlir.compile(model, input, output_type=format)
+        print(format, "\n", module.operation.get_asm(large_elements_limit=10))
