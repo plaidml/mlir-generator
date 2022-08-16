1b02114fe563072ea79ea94d17198b8134b64611
Renato Golin <rengolin@systemcall.eu>
2022-08-16 17:49:57 +0100

    fixing conv dimensions

--- front-ends/torch-mlir/models/conv.py
+++ front-ends/torch-mlir/models/conv.py
@@ -11,15 +11,14 @@ from torch_mlir_compile import torch_mlir_compile
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(3, 32, 3, 3)
+        self.conv2 = nn.Conv2d(32, 2, 3, 3)
 
     def forward(self, x):
             x = self.conv1(x)
             x = F.relu(x)
             x = self.conv2(x)
             x = F.relu(x)
-            x = F.max_pool2d(x, 2)
             output = F.log_softmax(x, dim=1)
             return output
 
@@ -27,7 +26,7 @@ def main():
     device = torch.device("cpu")
     simple = Net().to(device)
     # Any other dialect segfaults
-    torch_mlir_compile(simple, torch.ones(1, 3, 28, 28),['torch'])
+    torch_mlir_compile(simple, torch.ones(1, 3, 28, 28))
 
 if __name__ == '__main__':
     main()
