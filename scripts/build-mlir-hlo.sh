059887384d9e4016aa1e030d676e49ee0e5636ce
Renato Golin <rengolin@systemcall.eu>
2022-08-19 22:20:59 +0100

    add mlir-hlo

---
+++ scripts/build-mlir-hlo.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Builds MLIR-HLO following the following documentation:
+# https://github.com/tensorflow/mlir-hlo#quickstart-building-and-testing
+
+BUILD_TYPE=Release
+if [ "$1" == "-d" ]; then
+  echo "Building debug version"
+  BUILD_TYPE=Debug
+elif [ "$1" == "-rd" ]; then
+  echo "Building rel+debug version"
+  BUILD_TYPE=RelWithDebInfo
+fi
+
+# Run on container/remote directly, need to check
+PROJECT="mlir-generator"
+if [ -d "$PROJECT" ]; then
+  cd "$PROJECT"
+fi
+# Make sure the repo is in a good shape
+echo " + Updating submodules"
+git submodule update --depth 1 --init --recursive
+
+# Find patch directory
+PATCHES="$(git rev-parse --show-toplevel)/patches"
+if [ ! -d "$PATCHES" ]; then
+    echo "Cannot find patches directory"
+    exit 1
+fi
+
+# Go into mlir-hlo subrepo
+ROOT="$(git rev-parse --show-toplevel)/external/mlir-hlo"
+if [ ! -d "$ROOT" ]; then
+    echo "Cannot find repository root"
+    exit 1
+fi
+pushd "$ROOT"
+
+# Always grab a fresh env environment
+echo " + Creating a fresh venv"
+rm -rf mlir_venv
+python -m venv mlir_venv
+#echo "export PATH=$PATH:$ROOT/build/tools" >> mlir_venv/bin/activate
+#echo "export PYTHONPATH=$ROOT/build/compiler/bindings/python:$ROOT/build/runtime/bindings/python" >> mlir_venv/bin/activate
+echo "export CMAKE_GENERATOR=Ninja" >> mlir_venv/bin/activate
+source mlir_venv/bin/activate
+
+# Install Python dependencies
+echo " + Install Python dependencies"
+python -m pip install --upgrade pip
+# TODO: Install more stuff that we'll use when we do
+
+# Build LLVM on the right commit
+echo " + Building LLVM"
+LLVM_VERSION=$(cat build_tools/llvm_version.txt)
+git clone https://github.com/llvm/llvm-project.git
+pushd llvm-project
+git co main
+git br -D mlir-hlo
+git co -b mlir-hlo $LLVM_VERSION
+popd
+mkdir -p llvm-build
+./build_tools/build_mlir.sh ${PWD}/llvm-project/ ${PWD}/llvm-build
+
+# Path mlir-hlo (see: https://github.com/tensorflow/mlir-hlo/issues/46)
+echo " + Applying patches"
+if grep -q MLIRTosaDialect tosa/CMakeLists.txt; then
+  echo "Patch already applied"
+else
+  patch -p1 < "$PATCHES"/mlir-hlo-01.patch
+fi
+
+# Build mlir-hlo with LLVM in-tree
+echo " + Build mlir-hlo in-tree"
+rm -rf build
+mkdir build
+cmake -Bbuild -S . \
+   -DLLVM_ENABLE_LLD=ON \
+   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+   -DLLVM_ENABLE_ASSERTIONS=On \
+   -DMLIR_DIR=${PWD}/llvm-build/lib/cmake/mlir
+ninja -C build
+
+# Basic tests
+echo " + Run mlir-hlo tests"
+ninja -C build check-mlir-hlo
