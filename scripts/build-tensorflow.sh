06efd0371cb62cd417abed1d79d0297f1eceb711
Renato Golin <rengolin@systemcall.eu>
2022-08-26 14:49:00 +0100

    [WIP] Change tensorflow to build instead of install
    We need to get tools built not just the Python modules

--- scripts/build-tensorflow.sh
+++ scripts/build-tensorflow.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Installs TensorFlow on a virtual environment
+# Builds TensorFlow from source
+# https://www.tensorflow.org/install/source
 
 # Run on container/remote directly, need to check
 PROJECT="mlir-generator"
@@ -15,18 +16,41 @@ if [ ! -d "$ROOT" ]; then
     echo "Cannot find repository root"
     exit 1
 fi
-mkdir -p "$ROOT"
 pushd "$ROOT"
 
 # Always grab a fresh env environment
 echo " + Creating a fresh venv"
 rm -rf mlir_venv
 python -m venv mlir_venv
+PYTHON_VERSION=$(ls mlir_venv/lib)
+echo "export PYTHON_BIN_PATH=$ROOT/mlir_venv/bin/python" >> mlir_venv/bin/activate
+echo "export PYTHON_LIB_PATH=$ROOT/mlir_venv/lib/$PYTHON_VERSION/site-packages" >> mlir_venv/bin/activate
+echo "export TF_NEED_ROCM=0" >> mlir_venv/bin/activate
+echo "export TF_NEED_TENSORRT=0" >> mlir_venv/bin/activate
+echo "export TF_NEED_CUDA=0" >> mlir_venv/bin/activate
+echo "export TF_DOWNLOAD_CLANG=0" >> mlir_venv/bin/activate
+echo "export TF_SET_ANDROID_WORKSPACE=0" >> mlir_venv/bin/activate
 source mlir_venv/bin/activate
+
+# Bazel is a build dep, but installing bazel is... odd
+if [ ! -f bazelisk ]; then
+  wget -O bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.12.2/bazelisk-linux-amd64
+  chmod +x bazelisk
+fi
+cp bazelisk mlir_venv/bin/bazelisk
 
 # Install Python dependencies
 echo " + Install Python dependencies"
 python -m pip install --upgrade pip
-python -m pip install tensorflow keras
+python -m pip install numpy wheel packaging requests opt_einsum
+python -m pip install keras_preprocessing --no-deps
+
+# Configure
+echo " + Configure with venv python"
+echo "" | python configure.py
+
+# Build
+echo " + Building TensorFlow"
+bazelisk build //tensorflow/tools/pip_package:build_pip_package
 
 popd
