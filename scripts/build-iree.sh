5535928634a63afd2b6adb3af802ee08c7b12074
Renato Golin <rengolin@systemcall.eu>
2022-09-29 18:52:25 +0100

    add IREE+TPP build

--- scripts/build-iree.sh
+++ scripts/build-iree.sh
@@ -3,12 +3,23 @@
 # Builds IREE following the following documentation:
 # https://iree-org.github.io/iree/building-from-source/getting-started/
 # https://iree-org.github.io/iree/building-from-source/python-bindings-and-importers/
+# TPP build as in https://github.com/iree-org/iree/commit/63e12cafdf0bba3263bf2b5f74d7b2381f43af65
+
+SYNTAX="build_iree.sh [-tpp]"
+
+set -eu
+
+BUILD_WITH_TPP=False
+if [ $# -ge 1 ] && [ "$1" == "-tpp" ]; then
+  BUILD_WITH_TPP=True
+  shift
+fi
 
 BUILD_TYPE=Release
-if [ "$1" == "-d" ]; then
+if [ $# -ge 1 ] && [ "$1" == "-d" ]; then
   echo "Building debug version"
   BUILD_TYPE=Debug
-elif [ "$1" == "-rd" ]; then
+elif [ $# -ge 1 ] && [ "$1" == "-rd" ]; then
   echo "Building rel+debug version"
   BUILD_TYPE=RelWithDebInfo
 fi
@@ -30,18 +41,30 @@ if [ ! -d "$ROOT" ]; then
 fi
 pushd "$ROOT"
 
+BLD_DIR="$ROOT/build"
+git fetch
+if [ "$BUILD_WITH_TPP" == "True" ]; then
+  BLD_DIR="$BLD_DIR/tpp"
+  git reset --hard origin/tpp
+else
+  BLD_DIR="$BLD_DIR/main"
+  git reset --hard origin/main
+fi
+mkdir -p "$BLD_DIR"
+VENV_DIR="$BLD_DIR/venv"
+
 # Always grab a fresh env environment
 echo " + Creating a fresh venv"
-rm -rf mlir_venv
-python -m venv mlir_venv
-echo "export PATH=\$PATH:$ROOT/build/tools" >> mlir_venv/bin/activate
-echo "export PYTHONPATH=$ROOT/build/compiler/bindings/python:$ROOT/build/runtime/bindings/python" >> mlir_venv/bin/activate
-source mlir_venv/bin/activate
+rm -rf $VENV_DIR
+python -m venv $VENV_DIR
+echo "export PATH=\$PATH:$BLD_DIR/tools" >> $VENV_DIR/bin/activate
+echo "export PYTHONPATH=$BLD_DIR/compiler/bindings/python:$BLD_DIR/runtime/bindings/python" >> $VENV_DIR/bin/activate
+source $VENV_DIR/bin/activate
 
 # Install Python dependencies
 echo " + Install Python dependencies"
 python -m pip install --upgrade pip
-python -m pip install -r ./runtime/bindings/python/iree/runtime/build_requirements.txt
+python -m pip install -r $ROOT/runtime/bindings/python/iree/runtime/build_requirements.txt
 python -m pip install tensorflow iree-tools-tf keras
 
 # Checkout iree repos too
@@ -50,7 +73,14 @@ git submodule update --init --recursive
 
 # Build iree with LLVM in-tree
 echo " + Build iree in-tree"
-cmake -GNinja -Bbuild -S . \
+EXTRA_CMAKE_FLAGS=""
+if [ "$BUILD_WITH_TPP" == "True" ]; then
+  echo " + Adding TPP options"
+  EXTRA_CMAKE_FLAGS="-DIREE_USE_TPP=ON \
+                     -DCMAKE_C_FLAGS=-DIREE_HAL_EXECUTABLE_IMPORT_PROVIDER_DEFAULT_FN=iree_samples_tpp_import_provider \
+                     -DIREE_HAL_EXECUTABLE_LOADER_EXTRA_DEPS=iree_samples_tpp_import"
+fi
+cmake -GNinja -B$BLD_DIR -S $ROOT \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
@@ -62,16 +92,19 @@ cmake -GNinja -Bbuild -S . \
     -DIREE_ENABLE_ASSERTIONS=ON \
     -DIREE_BUILD_PYTHON_BINDINGS=ON \
     -DIREE_ENABLE_LLD=ON \
+     $EXTRA_CMAKE_FLAGS \
     -DPython3_EXECUTABLE=$(which python)
-ninja -C build
 
-# Basic tests
-ninja -C build iree-test-deps
-ctest --output-on-failure --parallel $(nproc) --test-dir build
+# Not everything works with TPP
+if [ "$BUILD_WITH_TPP" == "True" ]; then
+  ninja -C "$BLD_DIR" iree-opt iree-compile iree-run-module iree-benchmark-module
+else
+  ninja -C "$BLD_DIR"
+fi
 
 # Python bindings test
 echo " + Checking IREE Python bindings"
 python -c "import iree.compiler"
-python -c "import iree.runtime"
 
+deactivate
 popd
