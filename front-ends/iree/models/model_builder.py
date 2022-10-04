71c540f1ca105cf612b25a53436da9c980e4017b
Renato Golin <rengolin@systemcall.eu>
2022-10-04 11:48:27 +0100

    Dump inalg-on-tensors from IREE, still to stderr

--- front-ends/iree/models/model_builder.py
+++ front-ends/iree/models/model_builder.py
@@ -50,7 +50,10 @@ if __name__ == "__main__":
     # Compile the model using IREE
     compiler_module = tfc.compile_module(SequentialModule(), exported_names = ["predict"], import_only=True)
     backend = "llvm-cpu"
-    args = ["--iree-llvm-target-cpu-features=host", "--iree-mhlo-demote-i64-to-i32=false", "--iree-flow-demote-i64-to-i32"]
+    args = ["--iree-llvm-target-cpu-features=host",
+            "--iree-mhlo-demote-i64-to-i32=false",
+            "--iree-flow-demote-i64-to-i32",
+            "--mlir-print-ir-after=iree-mhlo-to-linalg-on-tensors"]
     backend_config = "local-task"
     flatbuffer_blob = compile_str(compiler_module, target_backends=[backend], extra_args=args, input_type="mhlo")
 
