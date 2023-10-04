ee3b466eab2cf555e60bddeb75d26bee94dfebc4
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2023-10-04 16:19:58 -0700

    Adding Linear, Conv, ResNet18 models with TorchDynamo
    1. Adding TorchDynamo based models for Linear, Conv, ResNet18
    2. Updating torch_mlir_compile to support TorchDynamo backend generation
    3. Fixing issue in generate.sh with changed paths

--- torch-script/lib/torch_mlir_compile.py
+++ torch-script/lib/torch_mlir_compile.py
@@ -1,10 +1,39 @@
 # Implements the torch_mlir compiler steps into multiple MLIR outputs
 
+from typing import List
+
 import torch
 import torch_mlir
+from torch_mlir.dynamo import make_simple_dynamo_backend
+from torch_mlir_e2e_test.linalg_on_tensors_backends import refbackend
 
 def torch_mlir_compile(model, input, output_formats=['torch', 'linalg-on-tensors', 'tosa', 'stablehlo']):
     pass
+    module = None
     for format in output_formats:
         module = torch_mlir.compile(model, input, output_type=format)
         print(format, "\n", module.operation.get_asm(large_elements_limit=10))
+    # Returning one of MLIR modules so that TorchDynamo can call the object.
+    # Otherwise, TorchDynamo does not get invoked.
+    # TorchScript models do not care for return from torch_mlir_compile.
+    return module
+
+
+@make_simple_dynamo_backend
+def refbackend_torchdynamo_backend(fx_graph: torch.fx.GraphModule,
+                                   example_inputs: List[torch.Tensor]):
+    mlir_module = torch_mlir_compile(
+        fx_graph, example_inputs, output_formats=['linalg-on-tensors'])
+    backend = refbackend.RefBackendLinalgOnTensorsBackend()
+    compiled = backend.compile(mlir_module)
+    loaded = backend.load(compiled)
+
+    def compiled_callable(*inputs):
+        inputs = [x.numpy() for x in inputs]
+        result = loaded.forward(*inputs)
+        if not isinstance(result, tuple):
+            result = torch.from_numpy(result)
+        else:
+            result = tuple(torch.from_numpy(x) for x in result)
+        return result
+    return compiled_callable
---
+++ torch-script/models/linear_torch_dynamo.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch._dynamo as dynamo
+
+import sys
+sys.path.append('../lib')
+from torch_mlir_compile import refbackend_torchdynamo_backend
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(128, 256)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+            x = self.fc1(x)
+            x = F.relu(x)
+            x = self.fc2(x)
+            x = F.relu(x)
+            output = F.log_softmax(x, dim=1)
+            return output
+
+
+def main():
+    device = torch.device("cpu")
+    simple = Net().to(device)
+    dynamo_callable = dynamo.optimize(refbackend_torchdynamo_backend)(simple)
+    dynamo_callable(torch.ones(2, 128))
+
+if __name__ == '__main__':
+    main()
